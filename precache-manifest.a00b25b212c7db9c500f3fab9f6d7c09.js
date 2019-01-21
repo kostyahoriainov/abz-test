537e702cd6cd101e0397efbc61f04e4c1c102d0c
kostyahoriainov <konstiantyn.horiainov@gmail.com>
2019-01-21 14:49:51 +0200

    Updates

---
+++ precache-manifest.a00b25b212c7db9c500f3fab9f6d7c09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cbc9b17feb57f8a09ce",
+    "url": "/abz-test/static/js/runtime~main.5cbc9b17.js"
+  },
+  {
+    "revision": "1550a61de6fec008630a",
+    "url": "/abz-test/static/js/main.1550a61d.chunk.js"
+  },
+  {
+    "revision": "067227e297e75ff0a730",
+    "url": "/abz-test/static/js/1.067227e2.chunk.js"
+  },
+  {
+    "revision": "e378d39b85c064d2e4187c76006483f3",
+    "url": "/abz-test/index.html"
+  }
+];
